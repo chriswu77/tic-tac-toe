d5d41a6689651b735e6e321c512694ec3e14a375
Chris Wu <cwu0569@gmail.com>
2020-09-03 03:09:54 -0700

    cleaned up code. Fixed images

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const gameBoard = (function (){
-    // Use this module to control the gameBoard
     let board = ['', '', '', '', '', '', '', '', ''];
 
-    // create factory functions for player
     const Player = (name, letter) => {
         return {
             name,
@@ -59,13 +57,10 @@ const gameBoard = (function (){
 
     const insertInBoard = (letter, index) => {
         board[index] = letter;
-        console.log(board);
     };
 
     const insertInData = (player, index) => {
         player.pushTurn(index);
-        console.log(player);
-        console.log(player.turns);
     };
 
     const checkIfEmpty = index => {
@@ -112,7 +107,6 @@ const UIController = (function () {
         filledCircle: '<img src="images/filled_circle.png">'
     };
 
-    // use this module to control the display 
     const cellDOMs = Array.from(document.querySelectorAll('.box'));
     const playerOneDot = document.querySelector('.player-one-dot');
     const playerTwoDot = document.querySelector('.player-two-dot');
@@ -124,14 +118,6 @@ const UIController = (function () {
     const modal = document.querySelector('.modal');
     const displayNames = Array.from(document.querySelectorAll('.player-text, .player-name'));
     const modalContent = document.querySelector('.modal-content');
-
-    // const setLetterStyle = function (letter, cell) {
-    //     if (letter === 'X') {
-    //         cell.classList.add('player-1-color');
-    //     } else if (letter === 'O') {
-    //         cell.classList.add('player-2-color');
-    //     }
-    // };
 
     // public functions
     const renderMove = board => {
@@ -166,10 +152,6 @@ const UIController = (function () {
     };
 
     const highlightWin = (winningCombo, letter) => {
-        // letter = letter.toLowerCase();
-        // winningCombo.forEach(index => {
-        //     document.getElementById(`box${index}`).classList.add(`highlight-${letter}`);
-        // });
         let img;
         letter === 'X' ? img = images.filledCross : img = images.filledCircle;
         winningCombo.forEach(index => {
@@ -178,14 +160,7 @@ const UIController = (function () {
     };
 
     const clearBoard = () => {
-        cellDOMs.forEach(cell => {
-            // cell.textContent = '';
-            cell.innerHTML = '';
-            cell.classList.remove('highlight-x');
-            cell.classList.remove('highlight-o');
-            cell.classList.remove('player-1-color');
-            cell.classList.remove('player-2-color');
-        });
+        cellDOMs.forEach(cell => cell.innerHTML = '');
     };
 
     const showDraw = () => {
@@ -199,11 +174,8 @@ const UIController = (function () {
     };
 
     const setWinner = letter => {
-        if (letter === 'X') {
-            winnerOne.textContent = 'WINNER!';
-        } else {
-            winnerTwo.textContent = 'WINNER!';
-        }
+        const text = 'WINNER!';
+        letter === 'X' ? winnerOne.textContent = text : winnerTwo.textContent = text;
     };
 
     const resetPlayers = () => {
@@ -217,7 +189,6 @@ const UIController = (function () {
     };
 
     const showNames = () => {
-        console.log(displayNames);
         displayNames.forEach(cur => cur.style.visibility = 'visible');
     }
 
@@ -259,7 +230,6 @@ const UIController = (function () {
 })();
 
 const gameController = (function () {
-    // use this module to control the flow of the game, Global app controller
     let gamePlaying;
     let clickCount;
     let mode;
@@ -268,7 +238,6 @@ const gameController = (function () {
 
     const setupEventListeners = () => {
         document.addEventListener('click', clickCell);
-        // document.querySelector('#player-form').addEventListener('submit', assignPlayer);
         document.getElementById('restart-btn').addEventListener('click', restart);
         Array.from(document.querySelectorAll('#vs-player-btn, #vs-computer-btn')).forEach(cur => cur.addEventListener('click', selectMode));
     };
@@ -294,42 +263,28 @@ const gameController = (function () {
     };
 
     const restart = () => {
-        // clear the game board
         gameBoard.clearBoard();
-        // clear the player turns
         playerX.clearTurns();
         playerO.clearTurns();
-        // reset click count
         clickCount = 0;
-        // set game state
         gamePlaying = true;
-        // reset the UI
         UIController.clearBoard();
         UIController.resetPlayers();
-        // UIController.firstPlayerDot();
         UIController.hideDraw();
     };
 
     const pushData = (player, cellIndex) => {
         gameBoard.insertInBoard(player.letter, cellIndex);
         gameBoard.insertInData(player, cellIndex);
-        // render the board to the UI
         UIController.renderMove(gameBoard.getBoard());
-        // UIController.toggleDot(player.letter);
     };      
 
     const gameOver = player => {
         const win = player.checkIfWin();
         const draw = player.checkIfDraw();
-        console.log(win);
-        console.log(draw);
-        // if win or draw don't allow anymore turns
         if (win.decision) {
             gamePlaying = false;
-            // set winner name and keep dot on winner
             UIController.setWinner(player.letter);
-            // highlight board UI for winning combo
-            // UIController.highlightWin(win.winningIndex, player.letter);
             UIController.highlightWin(win.winningIndex, player.letter);
         } else if (draw) {
             gamePlaying = false;
@@ -349,26 +304,20 @@ const gameController = (function () {
                         if (mode === 'player') {
                             let player;
                             clickCount++;
-                            console.log(cellIndex);
-                            console.log(clickCount);
                             if (clickCount % 2 === 1) {
-                                // Player X's turn
                                 player = playerX;
                             } else if (clickCount % 2 === 0) {
-                                // Player O's turn
                                 player = playerO;
                             }
                             pushData(player, cellIndex);
                             gameOver(player);
                         } else if (mode === 'computer') {
-                            // no need for click counter
                             // do players move first
                             pushData(playerX, cellIndex);
                             gameOver(playerX);
                             // do computers move right after. find an empty cell from gameboard
                             if (gamePlaying) {
                                 const emptyIndex = gameBoard.findEmptyCell();
-                                console.log(emptyIndex);
                                 pushData(playerO, emptyIndex);
                                 gameOver(playerO);
                             }
